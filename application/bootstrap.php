61b2ed028b2b479c2f395ceb5e1ae058e36f5100
Riu <radoslawmuszynski@gmail.com>
2012-10-26 13:12:20 +0200

    Wyłączenie modułów

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -96,10 +96,10 @@ Kohana::$config->attach(new Config\File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 Kohana::modules(array(
-	 'auth'       => MODPATH.'auth',       // Basic authentication
+	// 'auth'       => MODPATH.'auth',       // Basic authentication
 	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	'database'   => MODPATH.'database',   // Database access
+	// 'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
 	// 'minion'     => MODPATH.'minion',     // CLI Tasks
 	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
