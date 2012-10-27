c31dfafae0a9a3756d89c93addb5125a6c90f4e4
Riu <radoslawmuszynski@gmail.com>
2012-10-27 17:41:14 +0200

    Usunięcie niepotrzebnych plików.

--- system/classes/Kohana/UTF8/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Kohana\UTF8;
-/**
- * @package    Kohana
- * @category   Exceptions
- * @author     Kohana Team
- * @copyright  (c) 2009-2012 Kohana Team
- * @license    http://kohanaframework.org/license
- */
-class Exception extends \Kohana\Exception {}
